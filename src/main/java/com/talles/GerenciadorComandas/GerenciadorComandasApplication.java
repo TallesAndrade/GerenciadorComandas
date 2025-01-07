b4ca13947c90984d15b3e63794f85d6d1858b47e
Talles <tallesandrade7@gmail.com>
2025-01-07 17:39:51 -0300

    first commit

---
+++ src/main/java/com/talles/GerenciadorComandas/GerenciadorComandasApplication.java
@@ -0,0 +1,13 @@
+package com.talles.GerenciadorComandas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorComandasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorComandasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talles/GerenciadorComandas/GerenciadorComandasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talles.GerenciadorComandas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorComandasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
