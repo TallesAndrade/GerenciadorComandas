6655a29917582946b37f62a84b45a58cbe031de9
Talles Andrade Silva <tallesandrade7@gmail.com>
2025-05-01 18:30:11 -0300

    feat: adiciona métodos para devolução de estoque em diferentes contextos
    - Cria método voltarEstoque(Comanda) para devolução total ao cancelar comandas
    - Cria método devolverProdutoEstoque(...) para devolução parcial ao editar quantidade de itens

--- src/main/java/com/talles/GerenciadorComandas/service/ProdutoComandaService.java
+++ src/main/java/com/talles/GerenciadorComandas/service/ProdutoComandaService.java
@@ -2,10 +2,12 @@ package com.talles.GerenciadorComandas.service;
 
 import com.talles.GerenciadorComandas.entity.Comanda;
 import com.talles.GerenciadorComandas.entity.Estoque;
+import com.talles.GerenciadorComandas.entity.Produto;
 import com.talles.GerenciadorComandas.entity.ProdutoComanda;
 import com.talles.GerenciadorComandas.repository.EstoqueRepository;
 import com.talles.GerenciadorComandas.repository.ProdutoComandaRepository;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class ProdutoComandaService {
@@ -25,5 +27,23 @@ public class ProdutoComandaService {
             return produtoComandaRepository.save(new ProdutoComanda(comanda,produtoEstoque.getProduto(),quantidade));
         }
         return null;
+    }
+
+    public void voltarEstoque(Comanda comanda) {
+        List<ProdutoComanda> produtos = comanda.getProdutosComanda();
+        for(ProdutoComanda produtoComanda : produtos) {
+            Produto produto = produtoComanda.getProduto();
+            Estoque estoque = estoqueRepository.findById(produto.getId()).orElseThrow();
+            estoque.setQuantidade(estoque.getQuantidade() + produtoComanda.getQuantidade());
+            estoqueRepository.save(estoque);
+        }
+
+    }
+
+    public void devolverProdutoEstoque(ProdutoComanda produtoComanda,int quantidade) {
+        Produto produto = produtoComanda.getProduto();
+        Estoque estoque = estoqueRepository.findById(produto.getId()).orElseThrow();
+        estoque.setQuantidade(estoque.getQuantidade() + quantidade);
+        estoqueRepository.save(estoque);
     }
 }
