82101b4d0bff67bc033132b2045e5b6b6e280ae2
Talles Andrade Silva <tallesandrade7@gmail.com>
2025-05-01 18:32:22 -0300

    feat:adiciona  metodo no repository para buscar comanda por status

--- src/main/java/com/talles/GerenciadorComandas/repository/ComandaRepository.java
+++ src/main/java/com/talles/GerenciadorComandas/repository/ComandaRepository.java
@@ -1,7 +1,11 @@
 package com.talles.GerenciadorComandas.repository;
 
 import com.talles.GerenciadorComandas.entity.Comanda;
+import com.talles.GerenciadorComandas.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ComandaRepository extends JpaRepository<Comanda,Long> {
+    List<Comanda> findByStatusComanda(Status statusComanda);
 }
