37e56cdec3a3980ba34902633ee758601225ecf3
Talles Andrade Silva <tallesandrade7@gmail.com>
2025-05-22 18:53:29 -0300

    refactor:Atualiza o mapper manual para mapper com MapStruct

--- src/main/java/com/talles/GerenciadorComandas/mapper/ProdutoMapper.java
+++ src/main/java/com/talles/GerenciadorComandas/mapper/ProdutoMapper.java
@@ -1,27 +1,14 @@
 package com.talles.GerenciadorComandas.mapper;
 
-import com.talles.GerenciadorComandas.controller.dtos.ProdutoDTO;
+import com.talles.GerenciadorComandas.controller.dtos.ProdutoRequestDTO;
+import com.talles.GerenciadorComandas.controller.dtos.ProdutoResponseDTO;
 import com.talles.GerenciadorComandas.entity.Produto;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-@Component
-public class ProdutoMapper {
-    public Produto mapToEntity(ProdutoDTO produtoDTO) {
-        Produto produto = new Produto();
-        produto.setId(produtoDTO.getId());
-        produto.setNome(produtoDTO.getNome());
-        produto.setPreco(produtoDTO.getPreco());
-        produto.setCodigoDeBarras(produtoDTO.getCodigoDeBarras());
-        produto.setProdutoComandas(produtoDTO.getProdutoComandas());
-        return produto;
-    }
-    public ProdutoDTO mapToDTO(Produto produto){
-        ProdutoDTO produtoDTO = new ProdutoDTO();
-        produtoDTO.setId(produto.getId());
-        produtoDTO.setNome(produto.getNome());
-        produtoDTO.setPreco(produto.getPreco());
-        produtoDTO.setCodigoDeBarras(produto.getCodigoDeBarras());
-        produtoDTO.setProdutoComandas(produto.getProdutoComandas());
-        return produtoDTO;
-    }
+@Mapper(componentModel = "spring")
+public interface ProdutoMapper {
+    Produto toEntity(ProdutoRequestDTO requestDTO);
+
+    ProdutoResponseDTO toDTO(Produto produto);
+
 }
