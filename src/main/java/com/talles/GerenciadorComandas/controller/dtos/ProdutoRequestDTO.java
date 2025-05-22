ebdeef956c3ff7b309e4071cc24ec9c8edb4411a
Talles Andrade Silva <tallesandrade7@gmail.com>
2025-05-22 18:50:41 -0300

    refactor:Troca DTO generico para DTOs de entrada e saida

--- src/main/java/com/talles/GerenciadorComandas/controller/dtos/ProdutoRequestDTO.java
+++ src/main/java/com/talles/GerenciadorComandas/controller/dtos/ProdutoRequestDTO.java
@@ -1,25 +1,24 @@
 package com.talles.GerenciadorComandas.controller.dtos;
-
-import com.talles.GerenciadorComandas.entity.ProdutoComanda;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ProdutoRequestDTO {
+public class ProdutoRequestDTO{
 
 
+    @NotBlank
     private String nome;
-    private BigDecimal preco;
 
+    @NotNull
+    @DecimalMin("0.01")
+    private BigDecimal preco;
     private String codigoDeBarras;
 
-    private List<ProdutoComanda> produtoComandas = new ArrayList<>();
 }
--- src/main/java/com/talles/GerenciadorComandas/controller/dtos/ProdutoResponseDTO.java
+++ src/main/java/com/talles/GerenciadorComandas/controller/dtos/ProdutoResponseDTO.java
@@ -1,4 +1,18 @@
 package com.talles.GerenciadorComandas.controller.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ProdutoResponseDTO {
+    private Long id;
+    private String nome;
+    private BigDecimal preco;
+    private String codigoDeBarras;
 }
