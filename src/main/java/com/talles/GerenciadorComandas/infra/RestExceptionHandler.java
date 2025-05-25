51847e30543fb54169c54d40f56d3d11b02762c9
Talles Andrade Silva <tallesandrade7@gmail.com>
2025-05-25 13:57:44 -0300

    feat:adiciona novos tratamentos de erros

--- src/main/java/com/talles/GerenciadorComandas/infra/RestExceptionHandler.java
+++ src/main/java/com/talles/GerenciadorComandas/infra/RestExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.talles.GerenciadorComandas.infra;
 
+import com.talles.GerenciadorComandas.exceptions.EstoqueAindaDisponivelException;
+import com.talles.GerenciadorComandas.exceptions.EstoqueProdutoNotFoundException;
 import com.talles.GerenciadorComandas.exceptions.ProdutoNotFoundException;
+import com.talles.GerenciadorComandas.exceptions.QuantidadeInsuficienteException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,5 +19,18 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
+    @ExceptionHandler(EstoqueProdutoNotFoundException.class)
+    public ResponseEntity<String> estoqueProdutoNotFoundHandler(EstoqueProdutoNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
 
+    @ExceptionHandler(QuantidadeInsuficienteException.class)
+    public ResponseEntity<String> quantidadeInsuficienteHandler(QuantidadeInsuficienteException ex) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(EstoqueAindaDisponivelException.class)
+    public ResponseEntity<String> estoqueAIndaDisponivelHandler(EstoqueAindaDisponivelException ex) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(ex.getMessage());
+    }
 }
