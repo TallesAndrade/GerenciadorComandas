5d901a9ae38312adeb3726c1e003d3fa5c4c5dba
Talles Andrade Silva <tallesandrade7@gmail.com>
2025-05-25 14:13:14 -0300

    .

--- src/main/java/com/talles/GerenciadorComandas/controller/EstoqueController.java
+++ src/main/java/com/talles/GerenciadorComandas/controller/EstoqueController.java
@@ -1,7 +1,9 @@
 package com.talles.GerenciadorComandas.controller;
 
-import com.talles.GerenciadorComandas.controller.dtos.EstoqueDTO;
+import com.talles.GerenciadorComandas.controller.dtos.EstoqueRequestDTO;
+import com.talles.GerenciadorComandas.controller.dtos.EstoqueResponseDTO;
 import com.talles.GerenciadorComandas.service.EstoqueService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,16 +20,27 @@ public class EstoqueController {
     }
 
 
-    @PostMapping("/{id}/adicionarsaldo")
-    public ResponseEntity<EstoqueDTO> adicionarSaldoEstoque(@PathVariable Long id,@RequestBody EstoqueDTO estoqueDTO){
-     return ResponseEntity.status(HttpStatus.CREATED).body(estoqueService.adicionarQuantidadeEstoque(id, estoqueDTO));
+    @PatchMapping("/{id}/adicionarsaldo")
+    public ResponseEntity<EstoqueResponseDTO> adicionarSaldoEstoque(@PathVariable Long id,@Valid @RequestBody EstoqueRequestDTO estoqueDTO){
+     return ResponseEntity.status(HttpStatus.OK).body(estoqueService.adicionarQuantidadeEstoque(id, estoqueDTO));
     }
     @GetMapping("/{id}")
-    public ResponseEntity<EstoqueDTO> buscarEstoquePorProduto(@PathVariable Long id){
-        return ResponseEntity.status(HttpStatus.OK).body(estoqueService.listarProdutoEstoque(id));
+    public ResponseEntity<EstoqueResponseDTO> buscarPorProdutoId(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(estoqueService.buscarEstoquePorProdutoId(id));
     }
     @GetMapping
-    public ResponseEntity<List<EstoqueDTO>> listarEstoque(){
+    public ResponseEntity<List<EstoqueResponseDTO>> buscarTodos(){
        return ResponseEntity.status(HttpStatus.OK).body(estoqueService.listarEstoque());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable Long id){
+        estoqueService.deletarEstoque(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PatchMapping("/{id}/removersaldo")
+    public ResponseEntity<EstoqueResponseDTO> removerSaldo(@PathVariable long id, @Valid @RequestBody EstoqueRequestDTO estoqueDTO){
+        return ResponseEntity.status(HttpStatus.OK).body(estoqueService.subtrairQuantidadeEstoque(id,estoqueDTO.getQuantidade()));
     }
 }
