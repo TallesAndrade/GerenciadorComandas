049d292963c8a7c722562d54e41ac13c573b005a
Talles Andrade Silva <tallesandrade7@gmail.com>
2025-05-25 14:01:13 -0300

    feat:Cria novos DTOs de entrada e saida

--- src/main/java/com/talles/GerenciadorComandas/controller/dtos/EstoqueRequestDTO.java
+++ src/main/java/com/talles/GerenciadorComandas/controller/dtos/EstoqueRequestDTO.java
@@ -1,4 +1,17 @@
 package com.talles.GerenciadorComandas.controller.dtos;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EstoqueRequestDTO {
+
+    @NotNull
+    @Min(value = 1)
+    private int quantidade;
 }
--- src/main/java/com/talles/GerenciadorComandas/controller/dtos/EstoqueResponseDTO.java
+++ src/main/java/com/talles/GerenciadorComandas/controller/dtos/EstoqueResponseDTO.java
@@ -1,4 +1,15 @@
 package com.talles.GerenciadorComandas.controller.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EstoqueResponseDTO {
+    private Long id;
+    private String nomeProduto;
+    private Long idProduto;
+    private int quantidade;
 }
