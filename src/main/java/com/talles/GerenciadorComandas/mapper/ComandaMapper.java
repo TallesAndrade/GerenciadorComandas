7e1bda0096a933ad2263ae9d87078be9591e8857
Talles Andrade Silva <tallesandrade7@gmail.com>
2025-05-31 18:41:28 -0300

    refactor:remover mapeamento de resposta do ComandaMapper

--- src/main/java/com/talles/GerenciadorComandas/mapper/ComandaMapper.java
+++ src/main/java/com/talles/GerenciadorComandas/mapper/ComandaMapper.java
@@ -1,8 +1,6 @@
 package com.talles.GerenciadorComandas.mapper;
 
-import com.talles.GerenciadorComandas.controller.dtos.ComandaFechadaResponseDTO;
 import com.talles.GerenciadorComandas.controller.dtos.ComandaRequestDTO;
-import com.talles.GerenciadorComandas.controller.dtos.ComandaResponseDTO;
 import com.talles.GerenciadorComandas.entity.Comanda;
 import org.mapstruct.Mapper;
 
@@ -10,9 +8,7 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring" , uses = ProdutoComandaMapper.class)
 public interface ComandaMapper {
-    ComandaResponseDTO ToDto(Comanda comanda);
 
     Comanda ToEntity(ComandaRequestDTO comandaDTO);
 
-    ComandaFechadaResponseDTO toFechadaDTO(Comanda comanda);
 }
