904c1fa8c368d993fbdaf1dd0f2c8b1c968cafd1
Talles Andrade Silva <tallesandrade7@gmail.com>
2025-06-04 12:22:12 -0300

    feat:adiciona @NotNull @Min para validações

--- src/main/java/com/talles/GerenciadorComandas/controller/dtos/ItemComandaRequestDTO.java
+++ src/main/java/com/talles/GerenciadorComandas/controller/dtos/ItemComandaRequestDTO.java
@@ -1,9 +1,13 @@
 package com.talles.GerenciadorComandas.controller.dtos;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class ItemComandaRequestDTO {
+    @NotNull
     private Long idProduto;
+    @Min(1)
     private int quantidade;
 }
